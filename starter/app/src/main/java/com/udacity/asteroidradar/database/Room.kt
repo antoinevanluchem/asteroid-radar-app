46725f0e9121d15591d891d36995a5290cd60198
Antoine Van Luchem <antoine.van-luchem@bmw.de>
2024-06-10 11:36:31 +0200

    Fix room nullpointer bug

--- starter/app/src/main/java/com/udacity/asteroidradar/database/Room.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/database/Room.kt
@@ -1,3 +1,5 @@
+package com.udacity.asteroidradar.database
+
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
@@ -8,27 +10,20 @@ import com.udacity.asteroidradar.database.AsteroidDao
 @Database(entities = [Asteroid::class], version = 1)
 abstract class NasaDatabase : RoomDatabase() {
     abstract val asteroidDao: AsteroidDao
+}
 
-    companion object {
-        @Volatile
-        private var INSTANCE: NasaDatabase? = null
-        fun getInstance(context: Context): NasaDatabase {
-            synchronized(this) {
-                var instance = INSTANCE
-                if (instance == null) {
-                    instance = Room.databaseBuilder(
-                        context.applicationContext,
-                        NasaDatabase::class.java,
-                        "nasa_database"
-                    )
-                    .fallbackToDestructiveMigration()
-                    .build()
-                    INSTANCE = instance
-                }
-                return instance
-            }
+private lateinit var INSTANCE: NasaDatabase
+
+fun getDatabase(context: Context): NasaDatabase {
+    synchronized(NasaDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(context.applicationContext,
+                NasaDatabase::class.java,
+                "nasa_database").build()
         }
     }
+    return INSTANCE
 }
+
 
 
--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,17 +1,17 @@
 package com.udacity.asteroidradar.main
 
-import NasaDatabase
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import com.udacity.asteroidradar.database.getDatabase
 import com.udacity.asteroidradar.repository.AsteroidsRepository
 import kotlinx.coroutines.launch
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
 
-//    private val database = NasaDatabase.getInstance(application)
+    private val database = getDatabase(application).asteroidDao
     val asteroidsRepository = AsteroidsRepository()
 
     init {
